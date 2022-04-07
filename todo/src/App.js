8ffbed8c7b8adea5736724a964aa45bb7513c108
lukaszwojcik89 <wojcik.l@tlen.pl>
2022-04-07 18:35:22 +0200

    fix

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,103 +1,50 @@
 import './App.css';
-import { useEffect, useState } from "react";
-import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc, writeBatch } from 'firebase/firestore';
+import {useEffect, useState} from "react";
 
-import { loadFromLocalStorage, saveToLocalStorage } from "./utils/localstorage";
-import uuidGen from "./utils/uuid";
+import {collection, doc, writeBatch, onSnapshot} from "firebase/firestore";
 
 import Headline from "./components/Headline";
 import TaskInput from "./components/TaskInput";
 import TaskList from "./components/TaskList";
-import { db } from "./firebase";
-
+import {db} from "./firebase";
 
 function App() {
-    const [value, setValue] = useState('');
     const [tasks, setTasks] = useState([]);
     const [selection, setSelection] = useState('all');
 
-    const getData = async () => {
-        const querySnapshot = await getDocs(collection(db, "todos"));
-        setTasks(querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        })));
-    }
-
     useEffect(() => {
-        getData().catch(() => { });
-    }, []);
-
-    useEffect(() => {
-        setTasks(loadFromLocalStorage('tds'));
-    }, []);
-
-    useEffect(() => {
-        saveToLocalStorage('tds', tasks);
-    }, [tasks])
-
-    const handleChange = (event) => {
-        setValue(event.target.value);
-    }
-
-    const handleKeyUp = async (event) => {
-        if (event.key === 'Enter') {
-
-            const newToDo = {
-                name: value,
-                status: false
-            }
-
-            const docRef = await addDoc(collection(db, "todos"), newToDo);
-
-            setTasks([Object.assign(newToDo, { id: docRef.id }), ...tasks]);
-            setValue('');
-        }
-    }
-
-
-    async function handleChangeStatus(id) {
-
-        const newTasks = tasks.filter(task => task.id === id)[0];   
-        newTasks.status = !newTasks.status;
-
-        await updateDoc(doc(db, "todos", newTasks.id), { status: newTasks.status });
-
-        setTasks([...tasks]);
-    }
-
-    async function handleDeleteTask(id) {
-        await deleteDoc(doc(db, "todos", id));  // delete from firebase
-        setTasks(tasks.filter(task => task.id !== id))
-    }
-
-    async function handleDeleteDone() { 
-        const batch = writeBatch(db); // create a batch
-        tasks.forEach(task => { // loop through all tasks
-            if(task.status) {   // if task is done
-                const ref = doc(db, "todos", task.id); // get the reference
-                batch.delete(ref); // delete the task
-            }
+        const unsubscribe = onSnapshot(collection(db, 'todos'), (snapshot) => {
+            setTasks(snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            })))
         });
-        await batch.commit();
 
-        setTasks(tasks.filter(task => !task.status))
+        return () => {
+            unsubscribe();
+        }
+    }, []);
+
+    async function handleDeleteDone() {
+        const batch = writeBatch(db);
+        tasks.forEach(task => {
+            if (task.status) {
+                const ref = doc(db, 'todos', task.id);
+                batch.delete(ref);
+            }
+        })
+
+        await batch.commit();
     }
 
     return (
         <div className="App">
-            <Headline />
-            <TaskInput
-                value={value}
-                handleChange={handleChange}
-                handleKeyUp={handleKeyUp}
-            />
+            <Headline/>
+            <TaskInput/>
             {tasks.length === 0 ? ('') : (
                 <>
                     <TaskList
                         tasks={tasks}
-                        handleChangeStatus={handleChangeStatus}
-                        handleDeleteTask={handleDeleteTask}
                         selection={selection}
                     />
                     {/*TODO move to separate component */}
