2a33c56a62557b6134ec4af6467b9bde8a0a98cc
lukaszwojcik89 <wojcik.l@tlen.pl>
2022-04-12 16:02:31 +0200

    Working toodoapp

--- todo/src/components/TaskList.js
+++ todo/src/components/TaskList.js
@@ -1,16 +1,16 @@
 import TaskItem from "./TaskItem";
 
-function TaskList({tasks, selection}) {
+function TaskList({ tasks, selection }) {
     return (
         <ul>
             {tasks
                 .filter((e) => selection === 'all' || e.status === selection)
-                .map(({id, name, status}) => <TaskItem
-                key={id}
-                id={id}
-                status={status}
-                name={name}
-            />)}
+                .map(({ id, name, status }) => <TaskItem
+                    key={id}
+                    id={id}
+                    status={status}
+                    name={name}
+                />)}
         </ul>
     );
 }
